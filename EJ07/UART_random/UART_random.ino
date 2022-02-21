a72f97faae77002f455d64a9439dd19ae7469e1d
Ramon27802092 <jaramillo.ramon@hotmail.com>
2022-02-21 09:36:26 -0600

    Se agrega codigo arduino UART num aleatorios

---
+++ EJ07/UART_random/UART_random.ino
@@ -0,0 +1,38 @@
+/*
+  Multiple Serial test
+
+  Receives from the main serial port, sends to the others.
+  Receives from serial port 1, sends to the main serial (Serial 0).
+
+  This example works only with boards with more than one serial like Arduino Mega, Due, Zero etc.
+
+  The circuit:
+  - any serial device attached to Serial port 1
+  - Serial Monitor open on Serial port 0
+
+  created 30 Dec 2008
+  modified 20 May 2012
+  by Tom Igoe & Jed Roach
+  modified 27 Nov 2015
+  by Arturo Guadalupi
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/MultiSerialMega
+*/
+#include <stdint.h>
+#include <stdio.h>
+
+long randNumber;
+
+void setup() {
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+  randNumber = random(300);
+  Serial.println(randNumber);
+  delay(50);
+}
+  
